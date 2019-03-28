40b449a68d71c7e75a5308abaa82840094f9740b
Alberto <vielmagalberto@gmail.com>
2019-03-28 13:29:51 -0400

    sign up

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  
+  :address => '0.0.0.0:3000',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
